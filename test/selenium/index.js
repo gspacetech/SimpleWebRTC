7b3f85c8edb3539ec09c64099853b626c63fd649
Gaurav Parmar <gsp17feb@gmail.com>
2018-03-02 18:40:17 +0530

    Update index.js

--- test/selenium/index.js
+++ test/selenium/index.js
@@ -14,7 +14,7 @@ var path = require('path');
     var server=http.createServer(function(req,resp){
     // Print the name of the file for which request is made
        console.log("Request for demo file received.");
-       fs.readFile(path.join(__dirname, '..', 'test')+"/index.html",function(error,data){
+       fs.readFile(path.join(__dirname, 'test')+"/index.html",function(error,data){
            if (error) {
               resp.writeHead(404);
               resp.write('Contents you are looking for-not found'+path.join(__dirname, '..', 'test'));

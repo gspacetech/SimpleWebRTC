8bf27644d7339c4c4bb4d45277d4906aef4d6270
Gaurav Parmar <gsp17feb@gmail.com>
2018-03-02 17:33:23 +0530

    Update index.js

--- test/selenium/index.js
+++ test/selenium/index.js
@@ -1,7 +1,31 @@
 // Test 1-1 selenium.
-require('./p2p');
+//require('./p2p');
 
 // Test three-party mesh.
 // apparently too much for travis :-(
 // TODO: investigate sauce labs
 //require('./three');
+
+
+var http= require('http');
+
+    var fs= require('fs'),
+    port = parseInt(process.env.PORT || config.server.port, 10);
+    var server=http.createServer(function(req,resp){
+    // Print the name of the file for which request is made
+       console.log("Request for demo file received.");
+       fs.readFile("./index.htm",function(error,data){
+           if (error) {
+              resp.writeHead(404);
+              resp.write('Contents you are looking for-not found');
+              resp.end();
+                       }
+           else {
+              resp.writeHead(200, { 'Content-Type': 'text/html' });  
+              resp.write(data.toString());
+              resp.end();
+               }
+    });
+  });
+    server.listen(port); 
+    console.log('Server running at http://127.0.0.1:8081/'+ port);

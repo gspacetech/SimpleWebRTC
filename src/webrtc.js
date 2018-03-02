1f3144b5047f64cffd7ddc6c5315da84a009d484
Gaurav Parmar <gsp17feb@gmail.com>
2018-03-02 21:16:56 +0530

    Update webrtc.js

--- src/webrtc.js
+++ src/webrtc.js
@@ -12,17 +12,17 @@ function WebRTC(opts) {
             // makes the entire PC config overridable
             peerConnectionConfig: {
                 iceServers: [
-                    {'urls': 'stun:stun.l.google.com:19302'}
-                ,
+                    //{'urls': 'stun:stun.l.google.com:19302'}
+              //  ,
+    //{
+      //'urls': 'turn:192.158.29.39:3478?transport=udp',
+    //  'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+     // 'username': '28224511:1379330808'
+   // },
     {
-      'urls': 'turn:192.158.29.39:3478?transport=udp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
-    },
-    {
-      'urls': 'turn:192.158.29.39:3478?transport=tcp',
-      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      'username': '28224511:1379330808'
+      'urls': 'turn:turn.anyfirewall.com:443?transport=tcp',
+      'credential': 'webrtc',
+      'username': 'webrtc'
     }
                 ]
             },
